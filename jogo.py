889fbeb68c98608ec232551d27af5e1cc44659f0
schmidt22-renan <schmidt.renan@escola.pr.gov.br>
2025-07-31 12:17:50 +0000

    primeira parte do jogo

---
+++ jogo.py
@@ -0,0 +1,8 @@
+import rendom
+print("#########################")
+print("#  jogo de adivinhaçao  #")
+print("#########################")
+
+numeroSecreto= random.randrange(0,100)
+totalTentativas = 0
+pontos = 100
