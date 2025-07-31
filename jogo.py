c309a2eb6c925f23dabb9ad4a9a3b2bb3f2dcac9
schmidt22-renan <schmidt.renan@escola.pr.gov.br>
2025-07-31 14:51:52 +0000

    jogoadivinhaçao pt 2

--- jogo.py
+++ jogo.py
@@ -1,4 +1,4 @@
-import rendom
+import random
 print("#########################")
 print("#  jogo de adivinhaçao  #")
 print("#########################")
@@ -6,3 +6,18 @@ print("#########################")
 numeroSecreto= random.randrange(0,100)
 totalTentativas = 0
 pontos = 100
+print("Qual niveis de dificuldade?  ")
+print("(1) - facil (2) - medio (3) - dificil ")
+
+nivel = int(input("Escolha um nivel"))
+
+if(nivel == 1):
+  print("seu patinho ")
+  totaltentativas  = 20
+elif (nivel == 2):
+    print("fraco")
+    totaltentaivas = 10
+elif(nivel == 3):
+    print("konan")
+    totaltentativas = 5
+
